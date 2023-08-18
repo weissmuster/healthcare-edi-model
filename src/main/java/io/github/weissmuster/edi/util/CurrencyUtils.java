4e5c8d4fd1a5a71bec7c2b27a637e0ad8fe764a8
Thomas Wittig <weissmuster@gmail.com>
2023-08-18 17:28:46 +0200

    - add javadoc - remove unused stuff - update junit version

--- src/main/java/io/github/weissmuster/edi/util/CurrencyUtils.java
+++ src/main/java/io/github/weissmuster/edi/util/CurrencyUtils.java
@@ -7,6 +7,10 @@ import java.text.ParseException;
 import java.util.Locale;
 import org.apache.commons.lang.StringUtils;
 
+/**
+ * Utility class to provide various operations related to currency formatting and parsing
+ * specifically for the Euro (€) currency.
+ */
 public class CurrencyUtils {
 
   private static final String EURO = "€";
@@ -14,13 +18,21 @@ public class CurrencyUtils {
   private static final NumberFormat NUMBER_FORMAT =
       NumberFormat.getCurrencyInstance(Locale.GERMANY);
 
+  /**
+   * Formats the given BigDecimal amount to its currency representation.
+   *
+   * @param amount The BigDecimal amount to be formatted.
+   * @return The formatted currency string.
+   */
   public static String format(BigDecimal amount) {
     return format(amount.doubleValue());
   }
 
   /**
-   * @param amount
-   * @return
+   * Formats the given double amount to its currency representation.
+   *
+   * @param amount The double amount to be formatted. Can be null.
+   * @return The formatted currency string or null if the provided amount is null.
    */
   public static String format(Double amount) {
 
@@ -32,10 +44,11 @@ public class CurrencyUtils {
   }
 
   /**
-   * parse a currency string like: 2,33 € or 2,33 or 2.33 € or 2.33
+   * Parses the provided currency string into its double value representation. Handles various
+   * currency formats including "2,33 €", "2,33", "2.33 €", and "2.33".
    *
-   * @param value string to parse, maybe null
-   * @return null or double value
+   * @param value The string representation of the currency amount. Can be null.
+   * @return The parsed double value or null if parsing fails or the input string is null/blank.
    */
   public static Double parse(String value) {
 
@@ -58,9 +71,16 @@ public class CurrencyUtils {
     }
   }
 
+  /**
+   * Formats the given double amount to its currency representation without the Euro sign.
+   *
+   * @param amount The double amount to be formatted. Can be null.
+   * @return The formatted currency string without the Euro sign or null if the provided amount is
+   *     null.
+   */
   public static String formatNoEuroSign(Double amount) {
-    
-    if(amount == null) {
+
+    if (amount == null) {
       return null;
     }
 
@@ -69,6 +89,15 @@ public class CurrencyUtils {
     return tmp.substring(0, tmp.length() - 2);
   }
 
+  /**
+   * Rounds a given double value to the specified number of decimal places using HALF_UP rounding
+   * mode.
+   *
+   * @param value The double value to be rounded.
+   * @param places The number of decimal places to round to.
+   * @return The rounded double value.
+   * @throws IllegalArgumentException if the provided places is negative.
+   */
   public static double round(double value, int places) {
 
     if (places < 0) {
--- src/main/java/io/github/weissmuster/edi/util/EDIStringUtils.java
+++ src/main/java/io/github/weissmuster/edi/util/EDIStringUtils.java
@@ -2,9 +2,36 @@ package io.github.weissmuster.edi.util;
 
 import org.apache.commons.lang.StringUtils;
 
+/**
+ * Utility class for string manipulations.
+ *
+ * @author Thomas Wittig
+ */
 public class EDIStringUtils {
 
-  /** Escaped alle {+:'} mit einem ? for dem eigentlichen Zeichen */
+  /**
+   * Escapes special characters in the given string with a preceding '?'. The following characters
+   * are escaped:
+   *
+   * <ul>
+   *   <li>' (single quote) becomes ?'
+   *   <li>: becomes ?:
+   *   <li>, becomes ?,
+   *   <li>+ becomes ?+
+   * </ul>
+   *
+   * Additionally, accented characters are replaced as follows:
+   *
+   * <ul>
+   *   <li>é becomes e
+   *   <li>è becomes e
+   * </ul>
+   *
+   * If the input string is null or empty, an empty string is returned.
+   *
+   * @param str the input string to be escaped.
+   * @return the escaped string. If the input string is blank, returns an empty string.
+   */
   public static String escapeSpecialChars(String str) {
 
     if (StringUtils.isBlank(str)) {
